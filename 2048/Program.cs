253bd56f345addbd04fbdc80db52bcf4497d574b
阿星Plus <123@meowv.com>
2016-11-23 11:00:15 +0800

    2048 ConsoleApplication
    2048 ConsoleApplication

---
+++ 2048/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game _2048 = new Game();
+            _2048.Run();
+        }
+    }
+}
